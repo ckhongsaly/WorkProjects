c24914f69e3ed345089a7f99513e09eafbbb88a1
ckhongsaly <ckhongsa@asu.edu>
2019-05-07 13:21:09 -0700

    Added find and replace method for docx paragraph

--- src/main/java/SearchDocX/FindFile.java
+++ src/main/java/SearchDocX/FindFile.java
@@ -1,28 +1,63 @@
 package main.java.SearchDocX;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.commons.compress.archivers.dump.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
 
 public class FindFile {
 	
-	public static void findFile(String fileName, File directory) {
+	public static void findFile(File directory) {
 		File[] list = directory.listFiles();
 		
 		if(list != null) {
 			for (File file : list) {
 				if (file.isDirectory()) {
-					findFile(fileName, file);
-				}
-				else if (fileName.equalsIgnoreCase(file.getName())) {
 					System.out.println(file.getParentFile());
 				}
 			}
 		}
 	}
 	
-	public static void find_Replace_DocX() throws IOException, InvalidFormatException {
-		
+	public static void find_Replace_DocX(String path, String replacement) throws IOException, InvalidFormatException {
+		try {
+			File file = new File(path);
+			FileInputStream fis = new FileInputStream(file);
+			XWPFDocument document = new XWPFDocument(OPCPackage.open(fis));
+			
+			//Find and replace in paragraph
+			
+			List<XWPFParagraph> paragraphList = document.getParagraphs();
+			
+			for (XWPFParagraph paragraph : paragraphList) {
+				List<XWPFRun> runs = paragraph.getRuns();
+				if(runs != null) {
+					for (XWPFRun r: runs) {
+						String text = r.getText(0);
+						if (text != null && text.contains(replacement)) {
+							text = text.replace(replacement, ""); //remove text
+							r.setText(text,0);
+						}
+					}
+				}
+			}
+			
+			
+			
+			
+			//close FileInputSTream, XWPFDocument
+			fis.close();
+			document.close();
+			} 
+		catch (Exception ex) {
+				ex.printStackTrace();
+			}
 	}
-}
+	
+}//end of FindFile
