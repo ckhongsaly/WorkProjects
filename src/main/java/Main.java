9bc63a363da114b462b94e73c932e55ffc4c9b6c
ckhongsaly <ckhongsa@asu.edu>
2019-05-07 12:00:34 -0700

    Added poi library and readded checkstyle and spotbugs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,13 @@ package main.java;
 import main.java.ReadDocX.ReadDocX;
 
 public class Main {
-	private static final String PATH = "C:\\Users\\CatherineKhongsaly\\Dropbox\\Project_Workplace\\WorkProjects\\Files\\Test Template.docx";
-
+	private static final String PATH = "C:/Users/CatherineKhongsaly/Dropbox/Project_Workplace/WorkProjects/Files/Test Template.docx";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Class ReadDocx.");
-		//String path = "C:/Users/CatherineKhongsaly/Documents/Test/Test Template.docx";
 		
-		ReadDocX reader = new ReadDocX();
-		reader.readDocXFile(PATH);
+		ReadDocX.readDocXFile(PATH);
 	}
 	
 }
